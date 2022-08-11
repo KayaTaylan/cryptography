bc9a8f82f862a1438a663247c689b960a11a7cc8
KayaTaylan <taylankaya2@outlook.de>
2022-08-11 11:38:01 +0200

    Added UI and Decryption functionality
    A command-prompt UI has been added, as well as the functionality to decrypt the text. The text itself is no longer hard-coded, but instead read from the text.txt file

--- encryption.py
+++ encryption.py
@@ -1,15 +1,73 @@
 import string
 
-def caesar(text, shift, alphabets):
 
-    def shift_alphabet(alphabet):
-        return alphabet[shift:] + alphabet[:shift]
+#Saves the text in text.txt as variable
+text_file = open("text.txt", "rt")
+plain_text = text_file.read()
+text_file.close()
 
-    shifted_alphabet = tuple(map(shift_alphabet, alphabets))
-    final_alphabet = ''.join(alphabets)
-    final_shifted_alphabet = ''.join(shifted_alphabet)
-    table = str.maketrans(final_alphabet, final_shifted_alphabet)
-    return text.translate(table)
+#Deletes all entries in the text.txt file
+text_file = open("text.txt", "wt")
+text_file.write("")
+text_file.close()
 
-plain_text = "Hello World"
-print(caesar(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
+#Writes text into the specified file
+def write_to_file(text):
+    file = open("text.txt", "wt")
+    file.write(text)
+    file.close()
+
+print("-----------------------------------------------------------------------")
+print("------------------------Simple En-/Decryptor---------------------------")
+print("-----------------------------------------------------------------------")
+print('')
+user_input = input("Do you want to Encrypt OR Decrypt the file? (E/D) ")
+
+
+if(user_input == 'E' or user_input == 'e'):
+    print("Encrypting...")
+    #Encrypts the text that is saved as a variable 
+    def encrypt(text, shift, alphabets):
+
+        def shift_alphabet(alphabet):
+            return alphabet[shift:] + alphabet[:shift]
+
+        shifted_alphabet = tuple(map(shift_alphabet, alphabets))
+        final_alphabet = ''.join(alphabets)
+        final_shifted_alphabet = ''.join(shifted_alphabet)
+        table = str.maketrans(final_alphabet, final_shifted_alphabet)
+        return text.translate(table)
+    
+    encrypted_text = encrypt(plain_text, 3, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation])
+    write_to_file(encrypted_text)
+    print("The file has been encrypted!")
+elif (user_input == 'D' or user_input == 'd'):
+    print("Decrypting...")
+    #Decrypts the text that is saved as a variable 
+    def decrypt(text, shift, alphabets):
+
+        def shift_alphabet(alphabet):
+            return alphabet[shift:] + alphabet[:shift]
+
+        shifted_alphabet = tuple(map(shift_alphabet, alphabets))
+        final_alphabet = ''.join(alphabets)
+        final_shifted_alphabet = ''.join(shifted_alphabet)
+        table = str.maketrans(final_alphabet, final_shifted_alphabet)
+        return text.translate(table)
+
+    decrypted_text = decrypt(plain_text, 23, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation])
+    write_to_file(decrypted_text)
+    print("The file has been decrypted!")
+else:
+    print("Invalid option")
+    write_to_file(plain_text)
+        
+
+        
+
+
+
+
+
+
+
